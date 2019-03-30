f1461427c5c2891ff941e6c592d908fe9db82a6c
surfpals <46467476+surfpals@users.noreply.github.com>
2019-03-29 20:36:15 -0600

    second commit

--- public/makeapost.js
+++ public/makeapost.js
@@ -4,11 +4,8 @@ var app = new Vue({
     title: "",
     description: "",
     file: null,
-    addItem: null,
+    addPost: null,
     items: [],
-    findTitle: "",
-    findItem: null,
-
   },
   methods: {
     fileChanged(event) {
@@ -22,57 +19,14 @@ var app = new Vue({
 
           description: this.description,
         });
-        this.addItem = r2.data;
-        this.getItems();
+        this.addPost = r2.data;
+        this.getPosts();
       } catch (error) {
         console.log(error);
       }
     },
-    async getItems() {
-      try {
-        let response = await axios.get("/api/items");
-        this.items = response.data;
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    selectItem(item) {
-      this.findTitle = "";
-      this.findItem = item; //Sets findItem to the item they clicked on in the drop down
-    },
-    async deleteItem(item) {
-      console.log("Front end is going to delete the following item:");
-      console.log(item);
-      try {
-        await axios.delete("/api/items/" + item._id, item); //Tell it what item we want to delete
-        this.findItem = null;
-        this.getItems();
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async editItem(item) {
-      try {
-        await axios.put("/api/items/" + item._id, {
-          title: this.findItem.title,
-          description: this.findItem.description,
-        });
-        this.findItem = null;
-        this.getItems();
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
-    }
   },
   created: function() {
-    this.getItems();
+    this.getPosts();
   },
-  computed: {
-    suggestions() {
-      return this.items.filter(item => item.title.toLowerCase().startsWith(this.findTitle.toLowerCase()));
-    }
-  }
 });
